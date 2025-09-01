ccfe78bbe1bafc3865eaf251a27402ee4cbaeb7a
lethabo <shysnylethabo11@gmail.com>
2025-09-01 18:13:21 +0200

    fix: removed tempLogo.

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -3,7 +3,6 @@
 	import { onMount } from 'svelte';
 	import modeobserver from '$lib/utils/modeobserver';
 	import { Navbar, NavBrand } from 'flowbite-svelte';
-	import TempLogo from '$lib/ui/TempLogo.svelte';
 
 	let { children } = $props();
 </script>
@@ -15,7 +14,8 @@
 <Navbar class="bg-background z-50 flex w-full items-center justify-between lg:hidden">
 	<div class="flex items-center">
 		<NavBrand href="/">
-			<TempLogo />
+			<img src="/images/logo-icon.jpg" class="aspect-square h-8 rounded-full sm:h-10" alt="icon" />
+			<img src="/images/logo-raw.png" class="h-8 sm:h-10" alt="logo" />
 
 			<span class="hidden self-center text-xl font-semibold whitespace-nowrap dark:text-white"
 				>KasiKhaya</span
