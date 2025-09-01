5d7e1c381ebbd68f6de3405a249c1e9bdf04ccd6
lethabo <shysnylethabo11@gmail.com>
2025-09-01 18:21:35 +0200

    vercel please!!!!🙏🙏

--- src/routes/(main)/chat/+page.svelte
+++ src/routes/(main)/chat/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import VirtualConcierge from '$lib/ui/components/VirtualConcierge.svelte';
 	import { Button, Listgroup, ListPlaceholder, Search } from 'flowbite-svelte';
 	import { MicrophoneOutline } from 'flowbite-svelte-icons';
 	import { fade, scale } from 'svelte/transition';
