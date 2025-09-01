093bfbcd583bbe638cdafbdb92c171377d12a7a6
lethabo <shysnylethabo11@gmail.com>
2025-09-01 18:30:09 +0200

    fix: navbar image

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -20,8 +20,8 @@
 	<div class="flex items-center">
 		<NavHamburger />
 		<NavBrand href="/">
-			<TempLogo />
-
+			<img src="/images/logo-icon.jpg" class="aspect-square h-8 rounded-full sm:h-10" alt="icon" />
+			<img src="/images/logo-raw.png" class="h-8 sm:h-10" alt="logo" />
 			<span class="hidden self-center text-xl font-semibold whitespace-nowrap dark:text-white"
 				>KasiKhaya</span
 			>
